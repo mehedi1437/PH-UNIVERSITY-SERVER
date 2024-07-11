3fb66bb10ee6ad428fe046d4509983feda4566a5
mehediemon1437 <mdmehedihasanemon1437@gmail.com>
2024-07-11 14:50:00 +0600

    code refactor successfully

---
+++ src/app/admin/admin.route.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+
+import { AdminControllers } from './admin.controller';
+import { updateAdminValidationSchema } from './admin.validation';
+import validateRequest from '../middlewares/validateRequest';
+
+const router = express.Router();
+
+router.get('/', AdminControllers.getAllAdmins);
+
+router.get('/:id', AdminControllers.getSingleAdmin);
+
+router.patch(
+  '/:id',
+  validateRequest(updateAdminValidationSchema),
+  AdminControllers.updateAdmin,
+);
+
+router.delete('/:adminId', AdminControllers.deleteAdmin);
+
+export const AdminRoutes = router;
