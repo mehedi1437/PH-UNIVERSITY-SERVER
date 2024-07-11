450cdebc235216d61c14ca37177030434d94fe67
mehediemon1437 <mdmehedihasanemon1437@gmail.com>
2024-07-11 10:31:36 +0600

    module-14 done

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -36,7 +36,7 @@ class QueryBuilder<T> {
   }
 
   sort() {
-    const sort = this?.query?.sort || "-createdAt";
+    const sort = (this?.query?.sort as string)?.split(",")?.join(" ") || "-createdAt";
     this.modelQuery = this?.modelQuery?.sort(sort as string);
     return this;
   }
--- src/app/modules/student/student.serviece.ts
+++ src/app/modules/student/student.serviece.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import httpStatus from "http-status";
 import AppError from "../../errors/AppError";
 import { StudentModel } from "./student.model";
